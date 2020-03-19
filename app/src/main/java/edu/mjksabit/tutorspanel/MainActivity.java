663d04421ba676354c560515e0d8be79324725c7
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2020-03-19 22:08:16 +0600

    Added MultiScreen switching of app

--- app/src/main/java/edu/mjksabit/tutorspanel/MainActivity.java
+++ app/src/main/java/edu/mjksabit/tutorspanel/MainActivity.java
@@ -2,7 +2,9 @@ package edu.mjksabit.tutorspanel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showStudentList(View v) {
+        Intent intent = new Intent(MainActivity.this, StudentList.class);
+        startActivity(intent);
+    }
+
+    public void showRecordList(View v) {
+        Intent intent = new Intent(MainActivity.this, RecordList.class);
+        startActivity(intent);
+    }
+
+    public void showVisualizer(View v) {
+        Intent intent = new Intent(MainActivity.this, Visualizer.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/edu/mjksabit/tutorspanel/RecordList.java
@@ -0,0 +1,14 @@
+package edu.mjksabit.tutorspanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_list);
+    }
+}
