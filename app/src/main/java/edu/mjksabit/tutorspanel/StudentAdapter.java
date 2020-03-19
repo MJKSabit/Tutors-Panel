18668f1b42194f5d43263d1e40b918ea149ffff4
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2020-03-19 22:40:06 +0600

    Successful use of ListView and ArrayAdapter

---
+++ app/src/main/java/edu/mjksabit/tutorspanel/StudentAdapter.java
@@ -0,0 +1,35 @@
+package edu.mjksabit.tutorspanel;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class StudentAdapter extends ArrayAdapter<Student> {
+
+    public StudentAdapter(Context context, ArrayList<Student> students) {
+        super(context, 0, students);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Student student = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.student_list_template, parent, false);
+        }
+
+        TextView name = (TextView) convertView.findViewById(R.id.student_list_template_name_id);
+        name.setText(student.getId()+" "+student.getName());
+
+        return convertView;
+    }
+}
--- app/src/main/java/edu/mjksabit/tutorspanel/StudentList.java
+++ app/src/main/java/edu/mjksabit/tutorspanel/StudentList.java
@@ -3,6 +3,11 @@ package edu.mjksabit.tutorspanel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class StudentList extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class StudentList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Sifat"));
+        students.add(new Student(2, "Sabit"));
+        students.add(new Student(3, "Azmal"));
+        students.add(new Student(4, "Nabiha"));
+        students.add(new Student(5, "Yusha"));
+        students.add(new Student(6, "Hamza"));
+
+        ListView listView = (ListView) findViewById(R.id.student_list);
+        listView.setAdapter(new StudentAdapter(this.getBaseContext(), students));
+
     }
 }
